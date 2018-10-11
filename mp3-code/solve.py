80a5cfad611b9612e9eda2debe785591382b9f10
Hana Rimawi <hrimaw2@illinois.edu>
2018-10-10 21:25:14 -0500

    partial

--- mp3-code/solve.py
+++ mp3-code/solve.py
@@ -76,3 +76,112 @@ def solve(constraints):
     dim0 = len(constraints[0])
     dim1 = len(constraints[1])
     return np.random.randint(2, size=(dim0, dim1))
+
+
+"""
+	array([
+		[list([[4, 1]]), 
+		 list([[1, 1], [1, 1], [1, 1]]),
+         list([[3, 1], [1, 1]]), 
+		 list([[2, 1]]), 
+		 list([[1, 1], [1, 1]])],
+        [list([[2, 1]]), 
+		 list([[1, 1], [1, 1]]), 
+		 list([[3, 1], [1, 1]]),
+         list([[1, 1], [1, 1]]), 
+		 list([[5, 1]])]
+		], dtype=object)
+	
+	And a corresponding solution may be:
+
+	array([[0, 1, 1, 1, 1],
+		   [1, 0, 1, 0, 1],
+		   [1, 1, 1, 0, 1],
+		   [0, 0, 0, 1, 1],
+		   [0, 0, 1, 0, 1]])
+"""
+def row_checker(cols, constraints):
+	"""
+	assume cols is 2d array and constraints is list([[1, 1], [1, 1], [1, 1]])
+	return whether the columns are viable given the row constraints
+	NEEDS CONSECUTIVE CHEKKECEKKEKKCKEKCECCECKC
+	"""
+	space = 0
+	num_cols = len(cols)
+	row_pos = 0
+	for row in constraints:
+		print(row)
+		if len(row) == 1:
+			constraint = row[0]
+			quantity = constraint[0]
+			val = constraint[1]
+			quantity_count = 0
+			for col_pos in range(num_cols):
+				if cols[row_pos][col_pos] == val:
+					quantity_count += 1
+				if quantity_count > quantity:
+					return False
+			if quantity_count != quantity:
+				return False 
+		else:
+			for constraint in row:
+				space_checked = False
+				quantity = constraint[0]
+				val = constraint[1]
+				quantity_count = 0
+				for col_pos in range(num_cols):
+					if cols[row_pos][col_pos] == val:
+						quantity_count += 1
+					if quantity_count > quantity:
+						return False
+					#if we haven't checked for the space in between yet and we've reached the end of the block
+					if not space_checked and quantity == quantity_count:
+						space_checked = True
+						#check if the next col over is not the end and is not a space
+						if col_pos + 1 >= num_cols and cols[row_pos][col_pos+1] != space:
+							return False
+						else:
+							break
+				if quantity_count != quantity:
+					return False 
+		row_pos += 1
+	return True
+		
+
+def partial_row_checker(cols, constraints):
+	"""
+	assume cols is 2d array and constraints is list([[1, 1], [1, 1], [1, 1]])
+	return whether the columns are viable given the row constraints
+	NEEDS CONSECUTIVE CHEKKECEKKEKKCKEKCECCECKC
+	"""
+	space = 0
+	num_cols = len(cols)
+	row_pos = 0
+	for row in constraints:
+		quantity = 0
+		val = 1 #change later for colors
+		for constraint in row:
+			quantity += constraint[0]
+		quantity_count = 0
+		for col_pos in range(num_cols):
+			if cols[row_pos][col_pos] == val:
+				quantity_count += 1
+		if quantity_count > quantity:
+			return False
+		row_pos += 1
+	return True
+
+
+cols = [[0, 1, 1, 1, 1],
+		[1, 0, 1, 0, 1],
+		[1, 1, 1, 0, 1],
+	    [0, 0, 0, 1, 1],
+	    [0, 0, 1, 0, 1]]
+constraints = [list([[4, 1]]), 
+		 list([[1, 1], [1, 1], [1, 1]]),
+         list([[3, 1], [1, 1]]), 
+		 list([[2, 1]]), 
+		 list([[1, 1], [1, 1]])]
+
+# print(row_checker(cols, constraints))
+print(partial_row_checker(cols, constraints))
