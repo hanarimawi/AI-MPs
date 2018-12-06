b755866dbd2b3d0f05d3c89347c33fdda6be6d16
Hana Rimawi <hrimaw2@illinois.edu>
2018-12-06 13:12:38 -0600

    some q learning equation

--- mp7-code/agent.py
+++ mp7-code/agent.py
@@ -1,4 +1,5 @@
 import utils
+import random
 
 class Agent:
     
@@ -13,6 +14,12 @@ class Agent:
         self._num_actions = utils.NUM_ACTIONS
         # Create the Q Table to work with
         self.Q = utils.create_q_table()
+
+        self.epsilon = .1
+        self.alpha = .2
+        self.gamma = .9
+
+# https://github.com/studywolf/blog/blob/master/RL/Cat%20vs%20Mouse%20exploration/qlearn.py
 
     def act(self, state, bounces, done, won):
          #TODO - fill out this function
@@ -34,3 +41,34 @@ class Agent:
 
 
 
+
+    def getQ(self, state, action):
+        return self.Q.get((state, action), 0.0)
+        # return self.Q.get((state, action), 1.0)
+    
+    def learnQ(self, state, action, reward, value):
+        oldv = self.Q.get((state, action), None)
+        if oldv is None:
+            self.Q[(state, action)] = reward
+        else:
+            self.Q[(state, action)] = oldv + self.alpha * (value - oldv)
+
+    def chooseAction(self, state):
+        if random.random() < self.epsilon:
+            action = random.choice(self._actions)
+        else:
+            q = [self.getQ(state, a) for a in self._actions]
+            maxQ = max(q)
+            count = q.count(maxQ)
+            if count > 1:
+                best = [i for i in range(len(self._actions)) if self.Q[i] == maxQ]
+                i = random.choice(best)
+            else:
+                i = self.Q.index(maxQ)
+
+            action = self._actions[i]
+        return action
+
+    def learn(self, state1, action1, reward, state2):
+        maxqnew = max([self.getQ(state2, a) for a in self._actions])
+        self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
